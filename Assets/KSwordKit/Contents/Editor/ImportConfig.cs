986e781435c5c2cff408817b921d69ab9404a750
keenlovelife <ks@keenlove.com>
2020-07-23 19:22:14 +0800

    修改框架编辑器代码
    修改框架编辑器代码

--- Assets/KSwordKit/Contents/Editor/ImportConfig.cs
+++ Assets/KSwordKit/Contents/Editor/ImportConfig.cs
@@ -55,6 +55,70 @@ namespace KSwordKit.Contents.Editor
         /// <para>如果有些特殊文件需要在其他路径下才能正常工作，可以使用该项单独设置。</para>
         /// </summary>
         public List<ImportFileSetting> FileSettings;
+
+        public string CopyToFolder(string sourceDir, string destDir)
+        {
+            string error = "";
+            //try
+            //{
+            //    if (!System.IO.Directory.Exists(destDir))
+            //    {
+            //        System.IO.Directory.CreateDirectory(destDir);
+            //    }
+
+            //    var cinfo = new System.IO.DirectoryInfo(System.IO.Path.Combine(System.IO.Path.Combine(KSwordKitConst.KSwordKitContentsSourceDiretory, name)));
+            //    var icfnInfo = new System.IO.FileInfo(System.IO.Path.Combine(cinfo.FullName, ContentsEditor.ImportConfigFileName));
+
+            //    string[] fileList = System.IO.Directory.GetFiles(sourceDir, "*");
+            //    foreach (string f in fileList)
+            //    {
+            //        string fName = f.Substring(sourceDir.Length + 1);
+            //        if (useConfig)
+            //        {
+            //            var fileinfo = new System.IO.FileInfo(f);
+
+            //            if (fileinfo.FullName == icfnInfo.FullName)
+            //                continue;
+            //            var find = config.FileSettingList.Find((fs) =>
+            //            {
+            //                if (fs.IsDir || string.IsNullOrEmpty(fs.Path) || string.IsNullOrEmpty(fs.ImportPath)) return false;
+            //                if (new System.IO.DirectoryInfo(System.IO.Path.Combine(cinfo.FullName, fs.Path)).FullName == fileinfo.FullName) return true;
+            //                return new System.IO.FileInfo(fs.Path).FullName == fileinfo.FullName;
+            //            });
+
+            //            if (find != null)
+            //                continue;
+            //        }
+
+            //        EditorUtility.DisplayProgressBar(KSwordKitConst.KSwordKitName + ": 拷贝部件 " + name, "正在拷贝：" + fName, UnityEngine.Random.Range(0f, 1));
+            //        System.IO.File.Copy(System.IO.Path.Combine(sourceDir, fName), System.IO.Path.Combine(destDir, fName), true);
+            //    }
+            //    foreach (var dir in System.IO.Directory.GetDirectories(sourceDir, "*"))
+            //    {
+            //        if (useConfig)
+            //        {
+            //            var dirinfo = new System.IO.DirectoryInfo(dir);
+            //            var f = config.FileSettingList.Find((fs) =>
+            //            {
+            //                if (new System.IO.DirectoryInfo(System.IO.Path.Combine(cinfo.FullName, fs.ExamplePath)).FullName == dirinfo.FullName) return true;
+            //                if (!fs.IsDir || string.IsNullOrEmpty(fs.Path) || string.IsNullOrEmpty(fs.ImportPath)) return false;
+            //                return new System.IO.DirectoryInfo(System.IO.Path.Combine(cinfo.FullName, fs.Path)).FullName == dirinfo.FullName;
+            //            });
+            //            if (f != null)
+            //                continue;
+            //        }
+            //        error += CopyFolder(config, useConfig, name, dir, System.IO.Path.Combine(destDir, dir.Substring(sourceDir.Length + 1)));
+            //    }
+            //}
+            //catch (System.Exception e)
+            //{
+            //    error += e.Message;
+            //}
+
+            //EditorUtility.ClearProgressBar();
+
+            return error;
+        }
     }
 #endif
 }
--- Assets/KSwordKit/Contents/Editor/MakeNewComponentEditorWindow.cs
+++ Assets/KSwordKit/Contents/Editor/MakeNewComponentEditorWindow.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 
@@ -17,41 +18,163 @@ namespace KSwordKit.Contents.Editor
             window.Show();
         }
 
+        string defaultImportConfigContent = @"
+{
+    ""Name"":""#NewName#"",
+    ""Dependencies"":[],
+    ""ExampleFolderPaths"":[],
+    ""FileSettingList"":[]
+}
+";
         string newComponentPath = string.Empty;
         string name = "";
         string dependencies = "";
         string exampleFolderPaths = "";
+        string reSelect = "";
+        bool isCreateNew = false;
+        string newComonentContent = string.Empty;
+        bool errorloged;
         private void OnGUI()
         {
             EditorGUILayout.Space(10);
+            GUILayout.Label("新部件配置文件: " + newComponentPath);
+            reSelect = string.IsNullOrEmpty(newComponentPath) ? "已存在新部件配置文件" : "重新已存在的新部件配置文件";
             EditorGUILayout.BeginHorizontal();
-            name = EditorGUILayout.TextField("部件名称：", name);
-            EditorGUILayout.EndHorizontal();
-            EditorGUILayout.BeginVertical();
-            EditorGUILayout.LabelField("依赖：");
-            dependencies = EditorGUILayout.TextArea(dependencies, GUILayout.Height(100));
-            EditorGUILayout.EndVertical();
-            EditorGUILayout.BeginVertical();
-            EditorGUILayout.LabelField("示例代码位置：");
-            exampleFolderPaths = EditorGUILayout.TextArea(exampleFolderPaths, GUILayout.Height(100));
-            EditorGUILayout.EndVertical();
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField("特殊文件设置");
-            if (GUILayout.Button("添加"))
+            if (isCreateNew)
             {
+                if (GUILayout.Button("删除生成的配置文件"))
+                {
+                    System.IO.File.Delete(newComponentPath);
+                    System.IO.File.Delete(newComponentPath + ".meta");
+                    newComponentPath = string.Empty;
+                    reSelect = string.Empty;
+                    isCreateNew = false;
+                    AssetDatabase.Refresh();
+                }
+            }
+            else
+            {
+                if (GUILayout.Button(reSelect))
+                {
+                    newComponentPath = EditorUtility.OpenFilePanel("选择已存在的新部件配置文件", "Assets", "json");
+                }
+                if (reSelect == "重新已存在的新部件配置文件")
+                {
+                    if (GUILayout.Button("重置"))
+                    {
+                        newComponentPath = string.Empty;
+                        reSelect = string.Empty;
+                    }
+                }
+                else
+                {
+                    if (GUILayout.Button("创建新部件的配置文件"))
+                    {
+                        newComponentPath = EditorUtility.SaveFilePanel("保存配置文件", "Assets", "ImportConfig", "json");
+                        if (!string.IsNullOrEmpty(newComponentPath))
+                        {
+                            isCreateNew = true;
+                            System.IO.File.WriteAllText(newComponentPath, defaultImportConfigContent);
+                            AssetDatabase.Refresh();
+                        }
+                    }
+                }
+            }
+
+            EditorGUILayout.EndHorizontal();
+
+            if (!string.IsNullOrEmpty(newComponentPath))
+            {
+                newComonentContent = System.IO.File.ReadAllText(newComponentPath);
+                newComonentContent = EditorGUILayout.TextArea(newComonentContent, GUILayout.Height(200));
+                System.IO.File.WriteAllText(newComponentPath, newComonentContent);
+                ImportConfig config = null;
+                try
+                {
+                   config = JsonUtility.FromJson<ImportConfig>(newComonentContent);
+                  
+                }catch(System.Exception e)
+                {
+                    Debug.LogError(KSwordKitConst.KSwordKitName + ": json格式不正确！"+ e.Message);
+                    errorloged = true;
+                    return;
+                }
+                if (errorloged)
+                {
+                    errorloged = false;
+                    
+                    var assembly = System.Reflection.Assembly.GetAssembly(typeof(ActiveEditorTracker));
+                    var type = assembly.GetType("UnityEditorInternal.LogEntries");
+                    if (type == null)
+                    {
+                        type = assembly.GetType("UnityEditor.LogEntries");
+                    }
+                    var method = type.GetMethod("Clear");
+                    method.Invoke(new object(), null);
+                }
+                EditorGUILayout.Space(10);
+                EditorGUILayout.BeginVertical();
+                EditorGUILayout.LabelField("部件名称：", config.Name);
+                EditorGUILayout.LabelField("部件目录：", new System.IO.FileInfo(newComponentPath).Directory.FullName);
+                EditorGUILayout.EndVertical();
+                EditorGUILayout.BeginVertical();
+                EditorGUILayout.LabelField("依赖：");
+                var dependencies = string.Empty;
+                foreach(var d in config.Dependencies)
+                {
+                    if (string.IsNullOrEmpty(dependencies)) dependencies = d;
+                    else dependencies += "\n" + d;
+                }
+                if (string.IsNullOrEmpty(dependencies))
+                    dependencies = "\t无";
+                EditorGUILayout.LabelField(dependencies);
+                EditorGUILayout.EndVertical();
+                EditorGUILayout.BeginVertical();
+                EditorGUILayout.LabelField("示例代码位置：");
+                var exampleFolderPaths = string.Empty;
+                foreach (var e in config.ExampleFolderPaths)
+                {
+                    if (string.IsNullOrEmpty(exampleFolderPaths)) exampleFolderPaths = e;
+                    else exampleFolderPaths += "\n" + e;
+                }
+                if (string.IsNullOrEmpty(exampleFolderPaths))
+                    exampleFolderPaths = "\t无";
+                EditorGUILayout.LabelField(exampleFolderPaths);
+                EditorGUILayout.EndVertical();
+                EditorGUILayout.BeginVertical();
+                EditorGUILayout.LabelField("特殊文件设置: ");
+                var filesettings = string.Empty;
+                foreach (var s in config.FileSettings)
+                {
+                    var str = (s.IsDir ? "文件夹：" : "文件：") + s.Path + " -> " + s.ImportPath;
+                    if (string.IsNullOrEmpty(filesettings)) filesettings = str;
+                    else filesettings += "\n" + str;
+                }
+                if (string.IsNullOrEmpty(filesettings))
+                    filesettings = "\t无";
+                EditorGUILayout.LabelField(filesettings);
+                EditorGUILayout.EndVertical();
+                EditorGUILayout.BeginVertical();
+                if (GUILayout.Button("导出新部件"))
+                {
+                    string error;
+                    ExportNewComponent(newComponentPath, out error);
+                    EditorUtility.DisplayDialog("导出新部件 '" + config.Name + "' ", string.IsNullOrEmpty(error) ? "成功！":"失败："+ error , "确定");
+                }
+                EditorGUILayout.EndVertical();
+                EditorGUILayout.Space(10);
 
             }
-            EditorGUILayout.EndHorizontal();
-            EditorGUILayout.Space(10);
-
-            EditorGUILayout.BeginHorizontal();
-            GUILayout.Label("已存在新部件配置:");
-            if (GUILayout.Button("选择配置文件"))
-            {
-                newComponentPath = EditorUtility.OpenFilePanel("选择新部件的导入配置文件", "Assets", "json");
-            }
-            EditorGUILayout.EndHorizontal();
         }
+
+        void ExportNewComponent(string importConfigPath, out string error)
+        {
+            var dirinfo = new System.IO.FileInfo(newComponentPath).Directory;
+
+
+            error = null;
+        }
+
     }
 
 }
